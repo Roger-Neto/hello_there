9aa01e74b1640a83c5807cc08e485e176dc07154
Roger-Neto <jose.rogerio.n@gmail.com>
2021-09-15 11:09:28 -0300

    Adicionar arquivos de projeto.

---
+++ hello_there/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_there
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Rogério!");
+        }
+    }
+}
